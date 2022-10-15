8f18e2b50e35988cfcbf2995c7172a16e026300f
Jimmy van Hest <jimmyvanhest@gmail.com>
2022-10-15 12:10:31 +0200

    Restructured InterfacesState to a structure with methods.

--- src/lib.rs
+++ src/lib.rs
@@ -105,199 +105,102 @@ impl InterfaceState {
 }
 
 /// Maps an [InterfaceIndex] to an [InterfaceState]
-type InterfacesState = HashMap<InterfaceIndex, InterfaceState>;
-/// map [InterfacesState] to [InternetConnectivity]
-fn interfaces_state_to_internet_connectivity(state: &InterfacesState) -> InternetConnectivity {
-    let ipv4 = state
-        .values()
-        .any(|s| s.up && !s.ipv4.addresses.is_empty() && !s.ipv4.gateways.is_empty());
-    let ipv6 = state
-        .values()
-        .any(|s| s.up && !s.ipv6.addresses.is_empty() && !s.ipv6.gateways.is_empty());
-
-    match (ipv4, ipv6) {
-        (true, true) => InternetConnectivity::All,
-        (true, false) => InternetConnectivity::IpV4,
-        (false, true) => InternetConnectivity::IpV6,
-        (false, false) => InternetConnectivity::None,
-    }
+struct InterfacesState {
+    state: HashMap<InterfaceIndex, InterfaceState>,
 }
-
-#[derive(Debug, thiserror::Error)]
-enum CheckInternetConnectivityError {
-    #[error("an overrun occurred")]
-    Overrun(Vec<u8>),
-}
-
-/// Builds and updates an internal state with a subset of the information provided by rtnetlink.
-///
-/// From this state the internet connectivity with will be determined and send to tx.
-///
-/// This function will compete when the receiving end of tx is dropped.
-///
-/// # Errors
-///
-/// This function will return an error if any of the underlying rtnetlink requests return an error.
-async fn check_internet_connectivity(
-    handle: Handle,
-    mut messages: UnboundedReceiver<(NetlinkMessage<RtnlMessage>, SocketAddr)>,
-    mut tx: UnboundedSender<InternetConnectivity>,
-) -> Result<(), Error> {
-    let mut state = InterfacesState::new();
-
-    get_links(&handle, &mut state)
-        .await
-        .with_context(|| "get links failed")?;
-    get_addresses(&handle, &mut state)
-        .await
-        .with_context(|| "get addresses failed")?;
-    get_default_routes(&handle, IpVersion::V4, &mut state)
-        .await
-        .with_context(|| "get default routes ipv4 failed")?;
-    get_default_routes(&handle, IpVersion::V6, &mut state)
-        .await
-        .with_context(|| "get default routes ipv6 failed")?;
-
-    let mut conn = interfaces_state_to_internet_connectivity(&state);
-    tx.send(conn)
-        .await
-        .with_context(|| "sending initial connectivity state failed")?;
-
-    while let Some((message, _)) = futures::select! {
-        // TODO add some awaitable that resolves when the receiving side of tx is closed and let it resolve None in this select, resulting in the shutdown of this while loop
-        message = messages.next() => message,
-    } {
-        match &message.payload {
-            rtnetlink::proto::NetlinkPayload::Error(e) => {
-                return Err(rtnetlink::Error::NetlinkError(e.clone()))
-                    .with_context(|| "received rtnetlink error");
-            }
-            rtnetlink::proto::NetlinkPayload::Overrun(e) => {
-                return Err(CheckInternetConnectivityError::Overrun(e.clone()))
-                    .with_context(|| "an overrun was detected");
-            }
-            rtnetlink::proto::NetlinkPayload::InnerMessage(message) => match message {
-                rtnetlink::packet::RtnlMessage::NewLink(link) => {
-                    add_link(link, &mut state);
-                }
-                rtnetlink::packet::RtnlMessage::DelLink(link) => {
-                    remove_link(link, &mut state);
-                }
-                rtnetlink::packet::RtnlMessage::NewAddress(address) => {
-                    add_address(address, &mut state);
-                }
-                rtnetlink::packet::RtnlMessage::DelAddress(address) => {
-                    remove_address(address, &mut state);
-                }
-                rtnetlink::packet::RtnlMessage::NewRoute(route) => {
-                    add_default_route(route, &mut state);
-                }
-                rtnetlink::packet::RtnlMessage::DelRoute(route) => {
-                    remove_default_route(route, &mut state);
-                }
-                _ => {}
-            },
-            _ => {}
+impl InterfacesState {
+    fn new() -> Self {
+        Self {
+            state: HashMap::new(),
         }
+    }
+    /// convert to [InternetConnectivity]
+    fn internet_connectivity(&self) -> InternetConnectivity {
+        let ipv4 = self
+            .state
+            .values()
+            .any(|s| s.up && !s.ipv4.addresses.is_empty() && !s.ipv4.gateways.is_empty());
+        let ipv6 = self
+            .state
+            .values()
+            .any(|s| s.up && !s.ipv6.addresses.is_empty() && !s.ipv6.gateways.is_empty());
 
-        let new_conn = interfaces_state_to_internet_connectivity(&state);
-        if conn != new_conn {
-            conn = new_conn;
-            tx.send(conn)
-                .await
-                .with_context(|| "sending connectivity update failed")?;
+        match (ipv4, ipv6) {
+            (true, true) => InternetConnectivity::All,
+            (true, false) => InternetConnectivity::IpV4,
+            (false, true) => InternetConnectivity::IpV6,
+            (false, false) => InternetConnectivity::None,
         }
     }
 
-    Ok(())
-}
-
-/// Gets all interfaces from rtnetlink ignoring the loopback interfaces and records them in the [state](InterfacesState).
-///
-/// # Errors
-///
-/// This function will return an error if the underlying request has an error.
-async fn get_links(handle: &Handle, state: &mut InterfacesState) -> Result<(), Error> {
-    let mut links = handle.link().get().execute();
-
-    while let Some(link) = links.try_next().await? {
-        add_link(&link, state);
+    /// Adds a link entry
+    fn add_link(&mut self, link: &LinkMessage) {
+        if link.header.flags & IFF_LOOPBACK == 0 {
+            let s = self
+                .state
+                .entry(link.header.index)
+                .or_insert_with(|| InterfaceState::new(false));
+            s.up = link.header.flags & IFF_LOWER_UP != 0;
+        }
+    }
+    /// Removes a link entry
+    fn remove_link(&mut self, link: &LinkMessage) {
+        if link.header.flags & IFF_LOOPBACK == 0 {
+            self.state.remove(&link.header.index);
+        }
     }
 
-    Ok(())
-}
-/// Gets all addresses from rtnetlink and records them in the [state](InterfacesState).
-///
-/// # Errors
-///
-/// This function will return an error if the underlying request has an error.
-async fn get_addresses(handle: &Handle, state: &mut InterfacesState) -> Result<(), Error> {
-    let mut addresses = handle.address().get().execute();
-
-    while let Some(address) = addresses.try_next().await? {
-        add_address(&address, state);
-    }
-
-    Ok(())
-}
-/// Gets all default routes from rtnetlink for a specified [IpVersion] and records them in the [state](InterfacesState).
-///
-/// # Errors
-///
-/// This function will return an error if the underlying request has an error.
-async fn get_default_routes(
-    handle: &Handle,
-    ip_version: IpVersion,
-    state: &mut InterfacesState,
-) -> Result<(), Error> {
-    let mut routes = handle.route().get(ip_version).execute();
-
-    while let Some(route) = routes.try_next().await? {
-        add_default_route(&route, state);
-    }
-
-    Ok(())
-}
-
-/// Adds a link to [state](InterfacesState).
-fn add_link(link: &LinkMessage, state: &mut InterfacesState) {
-    if link.header.flags & IFF_LOOPBACK == 0 {
-        let s = state
-            .entry(link.header.index)
-            .or_insert_with(|| InterfaceState::new(false));
-        s.up = link.header.flags & IFF_LOWER_UP != 0;
-    }
-}
-/// Removes a link from [state](InterfacesState).
-fn remove_link(link: &LinkMessage, state: &mut InterfacesState) {
-    if link.header.flags & IFF_LOOPBACK == 0 {
-        state.remove(&link.header.index);
-    }
-}
-
-/// Adds an address to [state](InterfacesState).
-fn add_address(address: &AddressMessage, state: &mut InterfacesState) {
-    if let Some((index, ip_version, address)) = parse_address(address) {
-        let s = state
-            .entry(index)
-            .or_insert_with(|| InterfaceState::new(false));
-        match ip_version {
-            IpVersion::V4 => s.ipv4.addresses.insert(address),
-            IpVersion::V6 => s.ipv6.addresses.insert(address),
-        };
-    }
-}
-/// Removes an address from [state](InterfacesState).
-fn remove_address(address: &AddressMessage, state: &mut InterfacesState) {
-    if let Some((index, ip_version, address)) = parse_address(address) {
-        state.entry(index).and_modify(|state| {
+    /// Adds an address entry
+    fn add_address(&mut self, address: &AddressMessage) {
+        if let Some((index, ip_version, address)) = parse_address(address) {
+            let s = self
+                .state
+                .entry(index)
+                .or_insert_with(|| InterfaceState::new(false));
             match ip_version {
-                IpVersion::V4 => state.ipv4.addresses.remove(&address),
-                IpVersion::V6 => state.ipv6.addresses.remove(&address),
+                IpVersion::V4 => s.ipv4.addresses.insert(address),
+                IpVersion::V6 => s.ipv6.addresses.insert(address),
             };
-        });
+        }
+    }
+    /// Removes an address entry
+    fn remove_address(&mut self, address: &AddressMessage) {
+        if let Some((index, ip_version, address)) = parse_address(address) {
+            self.state.entry(index).and_modify(|state| {
+                match ip_version {
+                    IpVersion::V4 => state.ipv4.addresses.remove(&address),
+                    IpVersion::V6 => state.ipv6.addresses.remove(&address),
+                };
+            });
+        }
+    }
+
+    /// Adds a default route entry
+    fn add_default_route(&mut self, route: &RouteMessage) {
+        if let Some((index, ip_version, address, priority)) = parse_default_route(route) {
+            let s = self
+                .state
+                .entry(index)
+                .or_insert_with(|| InterfaceState::new(false));
+            match ip_version {
+                IpVersion::V4 => s.ipv4.gateways.insert((address, priority)),
+                IpVersion::V6 => s.ipv6.gateways.insert((address, priority)),
+            };
+        }
+    }
+    /// Removes a default route entry
+    fn remove_default_route(&mut self, route: &RouteMessage) {
+        if let Some((index, ip_version, address, priority)) = parse_default_route(route) {
+            self.state.entry(index).and_modify(|state| {
+                match ip_version {
+                    IpVersion::V4 => state.ipv4.gateways.remove(&(address, priority)),
+                    IpVersion::V6 => state.ipv6.gateways.remove(&(address, priority)),
+                };
+            });
+        }
     }
 }
+
 /// Extract useful information from an [AddressMessage].
 ///
 /// Has a valid result if the address is not permanent and actually has an address.
@@ -333,30 +236,6 @@ fn parse_address(addr: &AddressMessage) -> Option<(InterfaceIndex, IpVersion, Ip
         }
     } else {
         None
-    }
-}
-
-/// Adds a default route to [state](InterfacesState).
-fn add_default_route(route: &RouteMessage, state: &mut InterfacesState) {
-    if let Some((index, ip_version, address, priority)) = parse_default_route(route) {
-        let s = state
-            .entry(index)
-            .or_insert_with(|| InterfaceState::new(false));
-        match ip_version {
-            IpVersion::V4 => s.ipv4.gateways.insert((address, priority)),
-            IpVersion::V6 => s.ipv6.gateways.insert((address, priority)),
-        };
-    }
-}
-/// Removes a default route from [state](InterfacesState).
-fn remove_default_route(route: &RouteMessage, state: &mut InterfacesState) {
-    if let Some((index, ip_version, address, priority)) = parse_default_route(route) {
-        state.entry(index).and_modify(|state| {
-            match ip_version {
-                IpVersion::V4 => state.ipv4.gateways.remove(&(address, priority)),
-                IpVersion::V6 => state.ipv6.gateways.remove(&(address, priority)),
-            };
-        });
     }
 }
 /// Extract useful information from a [RouteMessage].
@@ -396,4 +275,140 @@ fn parse_default_route(
     } else {
         None
     }
+}
+
+#[derive(Debug, thiserror::Error)]
+enum CheckInternetConnectivityError {
+    #[error("an overrun occurred")]
+    Overrun(Vec<u8>),
+}
+
+/// Builds and updates an internal state with a subset of the information provided by rtnetlink.
+///
+/// From this state the internet connectivity with will be determined and send to tx.
+///
+/// This function will compete when the receiving end of tx is dropped.
+///
+/// # Errors
+///
+/// This function will return an error if any of the underlying rtnetlink requests return an error.
+async fn check_internet_connectivity(
+    handle: Handle,
+    mut messages: UnboundedReceiver<(NetlinkMessage<RtnlMessage>, SocketAddr)>,
+    mut tx: UnboundedSender<InternetConnectivity>,
+) -> Result<(), Error> {
+    let mut state = InterfacesState::new();
+
+    get_links(&handle, &mut state)
+        .await
+        .with_context(|| "get links failed")?;
+    get_addresses(&handle, &mut state)
+        .await
+        .with_context(|| "get addresses failed")?;
+    get_default_routes(&handle, IpVersion::V4, &mut state)
+        .await
+        .with_context(|| "get default routes ipv4 failed")?;
+    get_default_routes(&handle, IpVersion::V6, &mut state)
+        .await
+        .with_context(|| "get default routes ipv6 failed")?;
+
+    let mut conn = state.internet_connectivity();
+    tx.send(conn)
+        .await
+        .with_context(|| "sending initial connectivity state failed")?;
+
+    while let Some((message, _)) = futures::select! {
+        // TODO add some awaitable that resolves when the receiving side of tx is closed and let it resolve None in this select, resulting in the shutdown of this while loop
+        message = messages.next() => message,
+    } {
+        match &message.payload {
+            rtnetlink::proto::NetlinkPayload::Error(e) => {
+                return Err(rtnetlink::Error::NetlinkError(e.clone()))
+                    .with_context(|| "received rtnetlink error");
+            }
+            rtnetlink::proto::NetlinkPayload::Overrun(e) => {
+                return Err(CheckInternetConnectivityError::Overrun(e.clone()))
+                    .with_context(|| "an overrun was detected");
+            }
+            rtnetlink::proto::NetlinkPayload::InnerMessage(message) => match message {
+                rtnetlink::packet::RtnlMessage::NewLink(link) => {
+                    state.add_link(link);
+                }
+                rtnetlink::packet::RtnlMessage::DelLink(link) => {
+                    state.remove_link(link);
+                }
+                rtnetlink::packet::RtnlMessage::NewAddress(address) => {
+                    state.add_address(address);
+                }
+                rtnetlink::packet::RtnlMessage::DelAddress(address) => {
+                    state.remove_address(address);
+                }
+                rtnetlink::packet::RtnlMessage::NewRoute(route) => {
+                    state.add_default_route(route);
+                }
+                rtnetlink::packet::RtnlMessage::DelRoute(route) => {
+                    state.remove_default_route(route);
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+
+        let new_conn = state.internet_connectivity();
+        if conn != new_conn {
+            conn = new_conn;
+            tx.send(conn)
+                .await
+                .with_context(|| "sending connectivity update failed")?;
+        }
+    }
+
+    Ok(())
+}
+
+/// Gets all interfaces from rtnetlink ignoring the loopback interfaces and records them in the [state](InterfacesState).
+///
+/// # Errors
+///
+/// This function will return an error if the underlying request has an error.
+async fn get_links(handle: &Handle, state: &mut InterfacesState) -> Result<(), Error> {
+    let mut links = handle.link().get().execute();
+
+    while let Some(link) = links.try_next().await? {
+        state.add_link(&link);
+    }
+
+    Ok(())
+}
+/// Gets all addresses from rtnetlink and records them in the [state](InterfacesState).
+///
+/// # Errors
+///
+/// This function will return an error if the underlying request has an error.
+async fn get_addresses(handle: &Handle, state: &mut InterfacesState) -> Result<(), Error> {
+    let mut addresses = handle.address().get().execute();
+
+    while let Some(address) = addresses.try_next().await? {
+        state.add_address(&address);
+    }
+
+    Ok(())
+}
+/// Gets all default routes from rtnetlink for a specified [IpVersion] and records them in the [state](InterfacesState).
+///
+/// # Errors
+///
+/// This function will return an error if the underlying request has an error.
+async fn get_default_routes(
+    handle: &Handle,
+    ip_version: IpVersion,
+    state: &mut InterfacesState,
+) -> Result<(), Error> {
+    let mut routes = handle.route().get(ip_version).execute();
+
+    while let Some(route) = routes.try_next().await? {
+        state.add_default_route(&route);
+    }
+
+    Ok(())
 }
